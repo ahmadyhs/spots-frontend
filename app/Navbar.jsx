762b758df4c5fde5383a7b0ec7a73eed6b5f97cf
ahmadyhs <ahmad20022@mail.unpad.ac.id>
2023-05-06 11:49:00 +0700

    matching navbar with mockup design

--- app/Navbar.jsx
+++ app/Navbar.jsx
@@ -1,27 +1,34 @@
 import Link from 'next/link';
+import Image from 'next/image'
 
 export const Navbar = () => {
   return (
     <>
-      <nav className='flex items-center w-screen flex-wrap bg-[#17224D] p-3 '>
-        <Link className='inline-flex items-center p-2 mr-4 ' href='/'>
+      <nav className='flex justify-between w-full flex-wrap bg-[#17224D] py-2 px-6 rounded-lg'>
+        <div>
+        <Link className='inline-flex items-center' href='/landing'>
+          <Image src="/SPOTS-white-icon.svg" height={50} width={50}/>
         </Link>
-        <button className=' inline-flex p-3 hover:bg-blue-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none'>
-          
-        </button>
-        <div className='hidden w-full lg:inline-flex lg:flex-grow lg:w-auto'>
-          <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
-            <Link className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white ' href='/'>
+        </div>
+        
+        <div className='hidden w-full lg:inline-flex lg:flex-row lg:w-auto'>
+          <div className='lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col justify-center lg:h-auto'>
+            <Link className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:text-green-600' href='/'>
                 Daftarkan Ruangan
             </Link>
-            <Link className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white' href='/'>
+            <Link className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:text-green-600' href='/'>
                 Workspace 
             </Link>
-            <Link  className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white' href='/'>
+            <Link  className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:text-green-600' href='/'>
                 Bantuan
-            </Link>
-            <Link className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-[#19224E] font-bold items-center justify-center bg-[#FFFFFF] hover:text-white' href='/login'>
-                Login 
+            </Link>  
+          </div>
+        </div>
+
+        <div className='hidden w-full lg:inline-flex lg:flex-row lg:w-auto'>
+          <div className='lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col justify-center lg:h-auto'>
+            <Link className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-[#19224E] font-bold justify-center bg-[#FFFFFF] hover:bg-green-600 hover:text-white' href='/login'>
+                    Login 
             </Link>
           </div>
         </div>
