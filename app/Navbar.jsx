ed59e28fd40175c15cd5513ba486fcabc2649e8c
ahmadyhs <ahmad20022@mail.unpad.ac.id>
2023-05-18 20:51:05 +0700

    readjust navbar

--- app/Navbar.jsx
+++ app/Navbar.jsx
@@ -6,21 +6,18 @@ export const Navbar = () => {
     <>
       <nav className='flex justify-between w-full flex-wrap bg-[#17224D] py-2 px-6 rounded-lg'>
         <div>
-        <Link className='inline-flex items-center' href='/landing'>
+        <Link className='inline-flex items-center' href='/'>
           <Image src="/SPOTS-white-icon.svg" height={50} width={50}/>
         </Link>
         </div>
         
         <div className='hidden w-full lg:inline-flex lg:flex-row lg:w-auto'>
           <div className='lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col justify-center lg:h-auto'>
-            <Link className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:text-green-600' href='/'>
-                Daftarkan Ruangan
-            </Link>
-            <Link className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:text-green-600' href='/'>
-                Workspace 
+            <Link className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:text-green-600' href='/eksplorasi'>
+                Cari Coworking Space
             </Link>
             <Link  className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:text-green-600' href='/'>
-                Bantuan
+                Tentang Kami
             </Link>  
           </div>
         </div>
