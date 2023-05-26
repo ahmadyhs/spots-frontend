3fda10ca09c322f8f6ec4789d70fce7f9427a842
ahmadyhs <ahmad20022@mail.unpad.ac.id>
2023-05-26 21:28:06 +0700

    add userbutton after login

--- app/Navbar.jsx
+++ app/Navbar.jsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
-import Image from 'next/image'
+import Image from 'next/image';
+import UserButton from './components/userMenu'
 
 export const Navbar = () => {
   return (
@@ -7,7 +8,7 @@ export const Navbar = () => {
       <nav className='flex justify-between w-full flex-wrap bg-[#17224D] py-2 px-6 rounded-lg'>
         <div>
         <Link className='inline-flex items-center' href='/'>
-          <Image src="/SPOTS-white-icon.svg" height={50} width={50}/>
+          <Image alt='logo' src="/SPOTS-white-icon.svg" height={50} width={50}/>
         </Link>
         </div>
         
@@ -21,14 +22,8 @@ export const Navbar = () => {
             </Link>  
           </div>
         </div>
-
-        <div className='hidden w-full lg:inline-flex lg:flex-row lg:w-auto'>
-          <div className='lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col justify-center lg:h-auto'>
-            <Link className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-[#19224E] font-bold justify-center bg-[#FFFFFF] hover:bg-green-600 hover:text-white' href='/login'>
-                    Login 
-            </Link>
-          </div>
-        </div>
+      
+        <UserButton></UserButton>
       </nav>
     </>
   );
