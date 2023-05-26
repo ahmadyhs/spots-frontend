9eaaa77fcf2a37d3d8ee66bed1e8521068fbcb37
ahmadyhs <ahmad20022@mail.unpad.ac.id>
2023-05-26 21:33:38 +0700

    add get all cowork space data from backend

--- app/eksplorasi/page.js
+++ app/eksplorasi/page.js
@@ -1,117 +1,90 @@
+'use client'
+
 import { Navbar } from '../Navbar';
-import Image from 'next/image';
-import Link from 'next/link';
+import { useContext } from 'react';
+import AuthContext from '../api/authContext';
+import Card from '../components/listingCard'
 
 
-export default function eksplor() {
-    return (
-      <body className='bg-white'>
-        <title>Eksplorasi</title>
-        <div className='flex w-full flex-col items-center justify-between p-4 bg-white'>
-          <Navbar/>
-        </div>
+const Eksplorasi = async () => {
+  const context = useContext(AuthContext);
+  const authorization = "Bearer " + context.token;
+  const header = {'Authorization': authorization};
 
-        <p className='bg-white text-2xl text-[#17224D] py-8 pl-12 font-bold'>
-          Cari Workspace
-        </p>
-        <div className="relative flex w-full flex-wrap">
-          <input
-            type="search"
-            className="relative ml-12 block flex-auto rounded-full border border-solid border-neutral-300 bg-transparent bg-clip-padding px-5 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
-            placeholder="Cari Workspace"/>
+  let spaceResult;
 
-          <button
-            className="bg-[#17224D] mx-6 relative z-[2] rounded-full border-2 border-primary px-6 py-2 text-xs font-medium uppercase text-primary transition duration-150 ease-in-out hover:bg-green-500 focus:outline-none focus:ring-0"
-            type="button"
-            id="">
-            Cari
-          </button>
-        </div>
+  await fetch ('https://api.spotscoworking.live/coworking-spaces', {headers: header})
+  .then(response => response.text())
+  .then(result => {
+      let resultArray = JSON.parse(result);
+      spaceResult = resultArray.coworkingSpaces;
+      console.log("Data Coworking Space berhasil didapatkan")
+  })
+  .catch(error => console.log('error', error));
 
-        <div className='flex py-2 my-5 items-center'>
-          <p className='text-[#17224D] pl-12 mr-4 font-bold'>
-            Urutkan berdasarkan </p>
-          <button
-            type="button"
-            className="rounded-xl border-2 border-[#17224D] px-3 py-1 mx-2 font-medium text-neutral-800 hover:bg-neutral-100 active:bg-green-300">
-            Jarak
-          </button>
-          <button
-            type="button"
-            className="rounded-xl border-2 border-[#17224D] px-3 py-1 mx-2 font-medium text-neutral-800 hover:bg-neutral-100 active:bg-green-300">
-            Rating
-          </button>
-          <button
-            type="button"
-            className="rounded-xl border-2 border-[#17224D] px-3 py-1 mx-2 font-medium text-neutral-800 hover:bg-neutral-100 active:bg-green-300">
-            Terbaru
-          </button>
-          <button
-            type="button"
-            className="rounded-xl border-2 border-[#17224D] px-3 py-1 mx-2 font-medium text-neutral-800 hover:bg-neutral-100 active:bg-green-300">
-          Populer
-          </button>
-        </div>
-        
-        
-        <div className='flex bg-gray-100 p-2 my-5 mx-12 border border-[#17224D] rounded-xl'>
-          <div className="w-2/5 py-5 px-5 flex justify-center">
-            <Image alt='room' src="/room.png" className='max-h-full max-w-full' width={200} height={100} />
-          </div>
-          
-          <div className='w-2/5'>
-            <p className='text-xl text-[#17224D] px-2 py-4 font-bold'>
-              RuangMeeting.ID
-            </p>
-            <div className='flex items-center'>
-              <Image alt='pin' src="/pin.png" className='scale-50' width={55} height={71} />
-              <p className='text-gray-400 text-xl px-2'>Bandung</p>
-            </div>
-            <div className='flex items-center'>
-              <p className='text-gray-400 text-xl px-2'>Mulai dari</p>
-              <p className='text-2xl font-bold text-teal-500 mt'>Rp 350.000/Jam</p>
-            </div>
-          </div>
-          
-          <div className='w-1/5 px-2 flex items-center'>
-            <div>
-              <Link type="submit" class="block bg-teal-500 hover:bg-blue-400 text-white font-semibold rounded-full
-                px-10 py-3 my-3 w-full text-center" href='/detail'>Lihat Detail</Link>
-              <Link type="submit" class="block bg-blue-950 hover:bg-blue-400 text-white font-semibold rounded-full
-                px-10 py-3 my-3 w-full text-center" href='/booking'>Booking</Link>
-            </div>
-          </div>
-        </div>
 
-        <div className='flex bg-gray-100 p-2 my-5 mx-12 border border-[#17224D] rounded-xl'>
-          <div className="w-2/5 py-5 px-5 flex justify-center">
-            <Image alt='room' src="/room2.png" className='max-h-full max-w-full' width={200} height={100} />
-          </div>
-          
-          <div className='w-2/5'>
-            <p className='text-xl text-[#17224D] px-2 py-4 font-bold'>
-              Cowork
-            </p>
-            <div className='flex items-center'>
-              <Image alt='pin' src="/pin.png" className='scale-50' width={55} height={71} />
-              <p className='text-gray-400 text-xl px-2'>Sukabumi</p>
-            </div>
-            <div className='flex items-center'>
-              <p className='text-gray-400 text-xl px-2'>Mulai dari</p>
-              <p className='text-2xl font-bold text-teal-500 mt'>Rp 250.000/Jam</p>
-            </div>
-          </div>
-          
-          <div className='w-1/5 px-2 flex items-center'>
-            <div>
-              <Link type="submit" class="block bg-teal-500 hover:bg-blue-400 text-white font-semibold rounded-full
-                px-10 py-3 my-3 w-full text-center" href='/detail'>Lihat Detail</Link>
-              <Link type="submit" class="block bg-blue-950 hover:bg-blue-400 text-white font-semibold rounded-full
-                px-10 py-3 my-3 w-full text-center" href='/booking'>Booking</Link>
-            </div>
-          </div>
-        </div>
+  return (
+    <div>
+      <title>Eksplorasi</title>
+      <div className='flex w-full flex-col items-center justify-between p-4 bg-white'>
+        <Navbar/>
+      </div>
 
-        </body>
+      <p className='bg-white text-2xl text-[#17224D] py-8 pl-12 font-bold'>
+        Cari Workspace
+      </p>
+      <div className="relative flex w-full flex-wrap">
+        <input
+          type="search"
+          className="relative ml-12 block flex-auto rounded-full border border-solid border-neutral-300 bg-transparent bg-clip-padding px-5 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+          placeholder="Cari Workspace"/>
+
+        <button
+          className="bg-[#17224D] mx-6 relative z-[2] rounded-full border-2 border-primary px-6 py-2 text-xs font-medium uppercase text-primary transition duration-150 ease-in-out hover:bg-green-500 focus:outline-none focus:ring-0"
+          type="button"
+          id="">
+          Cari
+        </button>
+      </div>
+
+      <div className='flex py-2 my-5 items-center'>
+        <p className='text-[#17224D] pl-12 mr-4 font-bold'>
+          Urutkan berdasarkan </p>
+        <button
+          type="button"
+          className="rounded-xl border-2 border-[#17224D] px-3 py-1 mx-2 font-medium text-neutral-800 hover:bg-neutral-100 active:bg-green-300">
+          Jarak
+        </button>
+        <button
+          type="button"
+          className="rounded-xl border-2 border-[#17224D] px-3 py-1 mx-2 font-medium text-neutral-800 hover:bg-neutral-100 active:bg-green-300">
+          Rating
+        </button>
+        <button
+          type="button"
+          className="rounded-xl border-2 border-[#17224D] px-3 py-1 mx-2 font-medium text-neutral-800 hover:bg-neutral-100 active:bg-green-300">
+          Terbaru
+        </button>
+        <button
+          type="button"
+          className="rounded-xl border-2 border-[#17224D] px-3 py-1 mx-2 font-medium text-neutral-800 hover:bg-neutral-100 active:bg-green-300">
+        Populer
+        </button>
+      </div>
+      
+      {spaceResult.map(space =>{
+        return(
+          <Card
+            key={space.location.space_id}
+            name={space.name}
+            price={space.price}
+            place={space.location.address}
+            image={space.coworking_space_images[0].image_url} />
+          )
+        })
+      }
+    </div>
   )
 }
+
+export default Eksplorasi
