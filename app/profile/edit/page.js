673b4c1d9b1416448af24b364dc99fe91bc66785
ahmadyhs <ahmad20022@mail.unpad.ac.id>
2023-05-26 21:32:23 +0700

    add fetch profile data and redesign edit profile

--- app/profile/edit/page.js
+++ app/profile/edit/page.js
@@ -1,57 +1,68 @@
-import Image from 'next/image';
+
 import { Navbar } from '../../Navbar';
 
 export default function Edit() {
-    return (
-      <body className='bg-white'>
-        <title>Edit Profile</title>
-        <div className='flex w-full flex-col items-center justify-between p-4 bg-white'>
-        <Navbar/>
-        </div>
+  return (
+    <div className='bg-white'>
+      <title>Edit Profile</title>
+      <div className='flex w-full flex-col items-center justify-between p-4 bg-white'>
+      <Navbar/>
+      </div>
 
-        <p className='bg-white text-3xl text-[#17224D] py-8 pl-12 font-bold'>
-          User Profile
-        </p>
-        
+      <p className='bg-white text-3xl text-[#17224D] py-8 pl-12 font-bold'>
+        Edit Profile
+      </p>
+      
 
-        <div class="bg-teal-200 grid lg:grid-cols-3 py-20 items-center">
-          <div class="right flex flex-col items-center">
-            <div className='bg-white p-20 mt-10 rounded-full'>
-              <Image src="/person.png"  width={172} height={172} />
+      <div className="bg-teal-200 grid py-20 items-center">
+
+        <div className="px-20">
+          <form action="" method="">
+            <div className='flex flex-row items-center'>
+              <p className='w-3/12 text-[#17224D] text-2xl font-bold'>Nama Depan</p>
+              <input type="text" id="namaDepan"  
+              className="bg-transparent border border-[#17224D] rounded-2xl w-10/12 py-2 text-center text-black my-3 mr-10" ></input>
             </div>
-          </div>
 
-          <div className="col-span-2">
-          <form action="#" method="POST">
-              <div className='flex flex-row items-center'>
-                <p className='w-3/12 text-[#17224D] text-2xl font-bold'>First Name</p>
-                <input type="text" name="" id=""  
-                className="bg-transparent border border-[#17224D] rounded-2xl w-10/12 py-2 text-center text-black my-3 mr-10" autofocus autocomplete required></input>
-              </div>
+            <div className='flex flex-row items-center'>
+              <p className='w-3/12 text-[#17224D] text-2xl font-bold'>Nama Belakang</p>
+              <input type="text" id="namaBelakang"  
+              className="bg-transparent border border-[#17224D] rounded-2xl w-10/12 py-2 text-center text-black my-3 mr-10" ></input>
+            </div>
 
-              <div className='flex flex-row items-center'>
-                <p className='w-3/12 text-[#17224D] text-2xl font-bold'>Last Name</p>
-                <input type="text" name="" id=""  
-                className="bg-transparent border border-[#17224D] rounded-2xl w-10/12 py-2 text-center text-black my-3 mr-10" autofocus autocomplete required></input>
-              </div>
+            <div className='flex flex-row items-center'>
+              <p className='w-3/12 text-[#17224D] text-2xl font-bold'>Email</p>
+              <input type="email" id="email"  
+              className="bg-transparent border border-[#17224D] rounded-2xl w-10/12 py-2 text-center text-black my-3 mr-10" ></input>
+            </div>
 
-              <div className='flex flex-row items-center'>
-                <p className='w-3/12 text-[#17224D] text-2xl font-bold'>Email</p>
-                <input type="email" name="" id=""  
-                className="bg-transparent border border-[#17224D] rounded-2xl w-10/12 py-2 text-center text-black my-3 mr-10" autofocus autocomplete required></input>
+            <div className='flex flex-row items-center'>
+              <p className='w-3/12 text-[#17224D] text-2xl font-bold'>Nomor Telepon</p>
+              <input type="number" id="noTel"  
+              className="bg-transparent border border-[#17224D] rounded-2xl w-10/12 py-2 text-center text-black my-3 mr-10
+              [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none" ></input>
+            </div>
+            
+            <div className='flex flex-row items-center'>
+              <p className='w-3/12 text-[#17224D] text-2xl font-bold'>Foto</p>
+              <div className='rounded-xl bg-slate-400 h-20 w-8/12 border-dashed border-2 m-5'>
+                  <p className='bg-slate-100 p-2 text-black absolute my-4 ml-10'>Pilih File</p>
+                  <p className='text-black absolute my-6 ml-40 overflow-hidden'>silakan klik di sini</p>
+                  <input type="file" id="picture" accept='image/*'
+                          className="opacity-0 h-full w-full"/>
               </div>
-
-              <div className='flex flex-row items-center'>
-                <p className='w-3/12 text-[#17224D] text-2xl font-bold'>Phone Number</p>
-                <input type="number" name="" id=""  
-                className="bg-transparent border border-[#17224D] rounded-2xl w-10/12 py-2 text-center text-black my-3 mr-10
-                [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none" autofocus autocomplete required></input>
-              </div>
-              <button type="submit" class="block bg-blue-950 border border-teal-200 hover:bg-blue-400 text-white font-semibold rounded-lg
-                      px-20 py-3 m-auto mt-10">Edit</button>
-            </form>        
-          </div>
-        </div>  
-      </body>
-    )
+            </div>
+            
+            <div className='flex justify-center'>
+              <button type="submit" className="block bg-blue-950 border border-teal-200 hover:bg-blue-400 text-white font-semibold rounded-lg
+                    px-20 py-3 m-auto mt-10">Submit</button>
+              <a className="block bg-white border border-teal-200 hover:bg-blue-400 text-blue-950 font-semibold rounded-lg
+                      px-20 py-3 m-auto mt-10" href='/profile'>Kembali</a>
+            </div>
+                                  
+          </form>        
+        </div>
+      </div>  
+    </div>
+  )
 }
---
+++ app/profile/page.js
@@ -0,0 +1,75 @@
+'use client'
+
+import { Navbar } from '../Navbar';
+import { useContext } from 'react';
+import AuthContext from '../api/authContext';
+
+const Profile = async () => {
+  const context = useContext(AuthContext);
+  const authorization = "Bearer " + context.token;
+  const header = {'Authorization': authorization};
+
+  let profileData;
+  let picture;
+
+  console.log(header)
+
+  await fetch ('https://api.spotscoworking.live/tenants/profile', {headers: header})
+  .then(response => response.text())
+  .then(result => {
+      let resultArray = JSON.parse(result);
+      picture = resultArray.tenant.avatar_url;
+      profileData = resultArray.tenant.user;
+  })
+  .catch(error => console.log('error', error));
+
+  return (
+    <div className='bg-white'>
+      <title>User Profile</title>
+      <div className='flex w-full flex-col items-center justify-between p-4 bg-white'>
+      <Navbar/>
+      </div>
+
+      <p className='bg-white text-3xl text-[#17224D] py-8 pl-12 font-bold'>
+        User Profile
+      </p>
+      
+
+      <div className="bg-teal-200 grid lg:grid-cols-3 py-20 items-center">
+        <div className="right flex flex-col items-center">
+          <div className='bg-white p-20 mt-10 rounded-full'>
+            <img alt='profile-picture' src={picture}  width={172} height={172} />
+          </div>
+        </div>
+
+        <div className="col-span-2">
+          <div className='grid grid-cols-2 items-center my-3'>
+            <p className='w-full text-[#17224D] text-2xl font-bold'>Nama Depan</p>
+            <p className='w-full text-[#17224D] text-2xl font-bold'>{profileData.first_name}</p>
+          </div>
+
+          <div className='grid grid-cols-2 items-center my-3'>
+            <p className='w-full text-[#17224D] text-2xl font-bold'>Nama Belakang</p>
+            <p className='w-full text-[#17224D] text-2xl font-bold'>{profileData.lasst_name}</p>
+          </div>
+
+          <div className='grid grid-cols-2 items-center my-3'>
+            <p className='w-full text-[#17224D] text-2xl font-bold'>Email</p>
+            <p className='w-full text-[#17224D] text-2xl font-bold'>{profileData.email}</p>
+          </div>
+
+          <div className='grid grid-cols-2 items-center my-3'>
+            <p className='w-full text-[#17224D] text-2xl font-bold'>Nomor Telepon</p>
+            <p className='w-full text-[#17224D] text-2xl font-bold'>{profileData.phone_number}</p>
+          </div>
+          <div className='flex items-center'>
+            <a className=" bg-blue-950 border border-teal-200 hover:bg-blue-400 text-white font-semibold rounded-lg
+                  px-20 py-3 m-auto mt-10" href='/profile/edit'>Edit</a>
+          </div>     
+        </div>
+      </div>  
+    </div>
+  )
+}
+
+export default Profile
