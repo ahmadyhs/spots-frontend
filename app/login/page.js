4646d75a17a80331c1231291c534aa3ff14defa5
ahmadyhs <ahmad20022@mail.unpad.ac.id>
2023-06-04 16:32:02 +0700

    working auth before fix

--- app/login/page.js
+++ app/login/page.js
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { Toaster, toast } from 'react-hot-toast'
+import { FaWindowClose } from 'react-icons/fa'
 
 const Login = () => {
   const router = useRouter();
@@ -29,17 +30,28 @@ const Login = () => {
         toast.success('Login berhasil');
     }
     })
-    .catch(error => console.log('error', error));
+    .catch(error => {
+      console.log('error', error)
+      toast.error('Akun atau password salah')
+    });
   }
 
   return (
     <div className="h-screen">
       <title>Login</title>
       <Toaster/>
-      <div className="overflow-hidden bg-white grid lg:grid-cols-2 h-full">
-        <div className="bg-white right flex h-fit justify-center flex-col text-center">
-          <Image className="mx-auto scale-75" src="/spots-blue.png"
-          width={575} height={477} alt='logo' priority/>
+      <FaWindowClose 
+        className='bg-blue-950 absolute top-4 left-4 w-5 h-5 cursor-pointer'
+        onClick={
+          e => {
+            e.preventDefault();
+            router.push('/');
+          }
+        }/>
+      <div className=" bg-white grid lg:grid-cols-2 h-screen">
+        <div className="bg-white right flex h-screen justify-center flex-col text-center">
+          <Image className="mx-auto my-0" src="/spots-blue.png"
+          width={424} height={350} alt='logo' priority/>
 
           <h2 className="text-black text-xl font-semibold text-center mb-10">Masuk ke akun Anda!</h2>
           
@@ -51,25 +63,25 @@ const Login = () => {
             }>
             <div>
               <input type='text' placeholder="Email Anda" 
-              className="border rounded-xl w-11/12 py-2 text-center text-black m-3" required
+              className="border rounded-xl w-11/12 py-2 text-center text-black mx-3 my-1" required
               value={email}
               onChange={(e) => setEmail(e.target.value)}></input>
             </div>
 
             <div className="mt-4">
               <input type='password' placeholder="Password Anda"
-              className="border rounded-xl w-11/12 py-2 text-center text-black m-3" required
+              className="border rounded-xl w-11/12 py-2 text-center text-black mx-3 my-1" required
               value={password}
               onChange={(e) => setPassword(e.target.value)}></input>
             </div>
 
             <div className='flex justify-between mx-20 mb-10'>
               <button type="submit" className="block bg-blue-950 border hover:bg-blue-400 text-white font-semibold rounded-lg
-                    px-20 py-3 mt-6">
+                    xl:px-20 px-12 py-3 mt-6">
                 Masuk
               </button>        
               <a className="block bg-gray-100 border hover:bg-blue-400 text-blue-950 font-semibold rounded-lg
-                    px-20 py-3 mt-6"
+                    xl:px-20 px-12 py-3 mt-6"
                     href='/signup'>  
                   Daftar
               </a>   
@@ -77,7 +89,7 @@ const Login = () => {
           </form>
         </div>
 
-        <div className="">
+        <div className="md:visible invisible">
           <Image
           className="h-full w-full"
           src="/login.png"
